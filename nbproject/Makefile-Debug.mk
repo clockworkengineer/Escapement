3eb651bf181831fe13e5e0077020a32386876e8b
clockworkengineer <robert_tizzard@hotmail.com>
2018-08-18 15:08:42 +0100

    Addition of makefiles.

---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,105 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/Escapement.o \
+	${OBJECTDIR}/Escapement_CommandLine.o \
+	${OBJECTDIR}/Escapement_FileCache.o \
+	${OBJECTDIR}/Escapement_Files.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=../Antik/dist/Debug/GNU-Linux/libantik.a -lboost_system -lboost_filesystem -lboost_program_options `pkg-config --libs libcurl` `pkg-config --libs zlib` `pkg-config --libs openssl` -lpthread   
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/escapement
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/escapement: ../Antik/dist/Debug/GNU-Linux/libantik.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/escapement: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/escapement ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/Escapement.o: Escapement.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Antik/include -I../Antik/classes/implementation `pkg-config --cflags libcurl` `pkg-config --cflags zlib` `pkg-config --cflags openssl` -std=c++11  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Escapement.o Escapement.cpp
+
+${OBJECTDIR}/Escapement_CommandLine.o: Escapement_CommandLine.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Antik/include -I../Antik/classes/implementation `pkg-config --cflags libcurl` `pkg-config --cflags zlib` `pkg-config --cflags openssl` -std=c++11  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Escapement_CommandLine.o Escapement_CommandLine.cpp
+
+${OBJECTDIR}/Escapement_FileCache.o: Escapement_FileCache.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Antik/include -I../Antik/classes/implementation `pkg-config --cflags libcurl` `pkg-config --cflags zlib` `pkg-config --cflags openssl` -std=c++11  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Escapement_FileCache.o Escapement_FileCache.cpp
+
+${OBJECTDIR}/Escapement_Files.o: Escapement_Files.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Antik/include -I../Antik/classes/implementation `pkg-config --cflags libcurl` `pkg-config --cflags zlib` `pkg-config --cflags openssl` -std=c++11  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Escapement_Files.o Escapement_Files.cpp
+
+# Subprojects
+.build-subprojects:
+	cd ../Antik && ${MAKE}  -f Makefile CONF=Debug
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+
+# Subprojects
+.clean-subprojects:
+	cd ../Antik && ${MAKE}  -f Makefile CONF=Debug clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
